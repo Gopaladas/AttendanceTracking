6fa8dc645aeeaae15f16e88a7ddeb76133b4ac1d
umar-md <20r21a12f5@mlrinstitutions.ac.in>
2025-12-26 12:22:09 +0530

    fix base issues

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,17 +6,15 @@ import Attendance from "./pages/employee/Attendance";
 import NotFound from "./pages/NotFound";
 import Dashboard from "./pages/employee/Dashboard";
 import LeaveManagement from "./pages/employee/LeaveManagement";
-<<<<<<< HEAD
+
 import StatusTracker from "./pages/employee/StatusTracker";
 import LeaveStatus from "./pages/employee/LeaveStatus";
-=======
 
 // A simple helper to check if user is logged in
 const ProtectedRoute = ({ children }) => {
   const token = localStorage.getItem("token");
   return token ? children : <Navigate to="/login" />;
 };
->>>>>>> 2aae2ab426209b57fd4ce410be167c9a6dec1979
 
 function App() {
   return (
@@ -28,15 +26,14 @@ function App() {
       <Route path="/signup" element={<Signup />} />
       <Route path="/forgot-password" element={<ForgotPassword />} />
 
-<<<<<<< HEAD
       {/* Employee Routes (Direct Access) */}
       <Route path="/employee/dashboard" element={<Dashboard />} />
       <Route path="/employee/attendance" element={<Attendance />} />
       <Route path="/employee/leave-management" element={<LeaveManagement />} />
       <Route path="/employee/status-tracker" element={<StatusTracker />} />
-      <Route path="/employee/leave-status" element={<LeaveStatus />} /> 
+      <Route path="/employee/leave-status" element={<LeaveStatus />} />
       {/* 404 Route - Always keep this at the bottom */}
-=======
+
       {/* Employee Routes (Protected) */}
       <Route
         path="/employee/dashboard"
@@ -66,8 +63,6 @@ function App() {
       {/* Admin Routes (To be created next) */}
       {/* <Route path="/admin/dashboard" element={<AdminDashboard />} /> */}
 
-      {/* 404 Route */}
->>>>>>> 2aae2ab426209b57fd4ce410be167c9a6dec1979
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
